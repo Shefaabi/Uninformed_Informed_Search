d5d52e9ea7b039c7e026ebce48803a1f2ae93a09
SaraTamer <sasooelbihery@gmail.com>
2024-05-03 00:00:40 +0300

    Add color to question 1 output + comments

--- question1.pl
+++ question1.pl
@@ -1,23 +1,22 @@
-% problem specific
+% 
+% Input query design:
+% search([[cell(0,0,yellow),null]],[],[cell(0,0,yellow), cell(0,1,yellow), cell(0,2,yellow),cell(0,3,red),cell(1,0,blue), cell(1,1,yellow), cell(1,2,blue),cell(1,3,yellow),cell(2,0,blue), cell(2,1,blue), cell(2,2,blue),cell(2,3,yellow),cell(3,0,blue), cell(3,1,blue), cell(3,2,blue),cell(3,3,yellow)],4).
 %
-%
-%
-% [cell(0,0,c), ] , N = row
-% col = len / row
-% x = row
-% y = column
+
 
 move(CurrenCell, NextCell, State, N):-
     left(CurrenCell, NextCell, State, N); right(CurrenCell, NextCell, State, N);
     up(CurrenCell, NextCell, State, N); down(CurrenCell, NextCell, State, N).
 
-left(cell(X, Y, _), NextCell, State, _):-
+left(cell(X, Y, Color), NextCell, State, N):-
+    length(State,L),
+    M is L / N,
     NewY is Y - 1,
     NewY >= 0,
     member(cell(X, NewY, NextColor), State),
     NextCell = cell(X, NewY, NextColor).
 
-right(cell(X, Y, _), NextCell, State, N):-
+right(cell(X, Y, Color), NextCell, State, N):-
     length(State,L),
     M is L / N,
     NewY is Y + 1,
@@ -25,14 +24,18 @@ right(cell(X, Y, _), NextCell, State, N):-
     member(cell(X, NewY, NextColor), State),
     NextCell = cell(X, NewY, NextColor).
 
-up(cell(X, Y, _), NextCell, State, _):-
+up(cell(X, Y, Color), NextCell, State, N):-
+    length(State,L),
+    M is L / N,
     NewX is X - 1,
     NewX >= 0,
     member(cell(NewX, Y, NextColor), State),
     NextCell = cell(NewX, Y, NextColor).
 
 
-down(cell(X, Y, _), NextCell, State, N):-
+down(cell(X, Y, Color), NextCell, State, N):-
+    length(State,L),
+    M is L / N,
     NewX is X + 1,
     NewX < N,
     member(cell(NewX, Y, NextColor), State),
@@ -41,6 +44,7 @@ down(cell(X, Y, _), NextCell, State, N):-
 % -------------------------------------
 % Rule to check if there is a cycle of the same color including 4 cells or more
 % Rule to check if there is a cycle where all cells have the same color
+
 cycle_of_same_color(VisitedCells) :-
     member([cell(X,Y,Color),_], VisitedCells), % Take a cell from the visited cells
     cycle_check(X, Y, Color, [(X,Y)], VisitedCells). % Start cycle check from this cell
@@ -52,8 +56,8 @@ cycle_check(X, Y, Color, [(X0,Y0)|Visited], VisitedCells) :-
     Length >= 4, % Ensure cycle length is at least 4
     neighbor(X,Y , X0,Y0), !, % Next cell is the starting cell
     \+ (member((X1,Y1), [(X0, Y0)|Visited]), member([cell(X1,Y1,Color1),_], VisitedCells), Color \= Color1), % Ensure all cells in the cycle have the same color
-     write("Cycle is found!"), nl,
-     write([(X0, Y0)|Visited]), nl.
+    write("Cycle is found!"), nl,
+    write([(X0, Y0)|Visited]), write(" form a cycle of "), write(Color), nl.
 
 % Recursive case: Continue exploring neighboring cells
 cycle_check(X, Y, Color, [(X0,Y0)|Visited], VisitedCells) :-
@@ -69,27 +73,30 @@ neighbor(X, Y, X1, Y) :- X1 is X+1.
 neighbor(X, Y, X, Y1) :- Y1 is Y+1.
 neighbor(X, Y, X1, Y) :- X1 is X-1, X1 >= 0.
 neighbor(X, Y, X, Y1) :- Y1 is Y-1, Y1 >= 0.
+
 % -------------------------------------
-% [[cell(x,y,c), parent] ]
+
+% base case to terminate when there is a cycle found and print it
 search(Open, Closed, Board, N):-
     getState(Open, [CurrentState,Parent], _), % Step 1 Pop a state from the open list so that it becomes the current state.
     cycle_of_same_color(Closed) ,!.
 
+% base case to terminate if there is no cycles found
 search([], _, _, _) :-
     write("Search is complete!"), nl,
     write("No Cycle is found!"), !.
 
 search(Open, Closed, Board, N):-
     getState(Open, CurrentNode, TmpOpen),
-    getAllValidChildren(CurrentNode,TmpOpen,Closed,Children, Board, N), % Step3 Get the list of all valid next states (children) from the current state.
-    addChildren(Children, TmpOpen, NewOpen), % Step 4 Add the next states to the open list.
-    append(Closed, [CurrentNode], NewClosed), % Step 5.1 Add the current state to the list of visited states (closed list)
-    search(NewOpen, NewClosed, Board, N). % Step 5.2 then go to step 1.
+    getAllValidChildren(CurrentNode,TmpOpen,Closed,Children, Board, N), % Step 2 Get the list of all valid next states (children) from the current state.
+    addChildren(Children, TmpOpen, NewOpen), % Step 3 Add the next states to the open list.
+    append(Closed, [CurrentNode], NewClosed), % Step 4.1 Add the current state to the list of visited states (closed list)
+    search(NewOpen, NewClosed, Board, N). % Step 4.2 then go to step 1.
 
 getNextState([CurrenCell,_], Open, Closed, [NextCell, CurrenCell], Board, N):-
     move(CurrenCell, NextCell, Board, N),
     not(member([NextCell,_], Open)),
-    not(member([NextCell,_], Closed)). % Step 2 Check whether the current state is a goal state.
+    not(member([NextCell,_], Closed)). 
 
 % Implementation of getState and addChildren determine the search alg.
 % BFS
@@ -97,15 +104,8 @@ getState([],[_,_],[]):-!.
 getState([CurrentNode|Rest], CurrentNode, Rest).
 
 addChildren(Children, Open, NewOpen):-
-    append(Open, Children, NewOpen). % bfs
+    append(Open, Children, NewOpen). 
 
-% Implementation of step 3 to get the next states
-% valid = = > the state is not repeated and
-% does not violate the rules of the problem
 getAllValidChildren(Node, Open, Closed, Children, Board, N):-
     findall(Next, getNextState(Node, Open, Closed, Next, Board, N), Children).
-
-
-
-
 
--- question2.pl
+++ question2.pl
@@ -1,3 +1,10 @@
+
+%
+% The sample Input
+% search([[cell(0,0,red),null,0,x,0]],[],cell(1,3,red),[cell(0,0,red), cell(0,1,red), cell(0,2,yellow),cell(0,3,yellow),cell(1,0,red), cell(1,1,blue), cell(1,2,red),cell(1,3,red),cell(2,0,red), cell(2,1,red), cell(2,2,red),cell(2,3,yellow),cell(3,0,blue), cell(3,1,red), cell(3,2,blue),cell(3,3,yellow)],4).
+%
+
+% calculate the heuristic rule according to how far the current cell from the goal
 calculateH(cell(X,Y,Color),cell(Goalx,Goaly,_),H):-
     H is abs(X - Goalx) + abs(Y - Goaly).
 
@@ -34,7 +41,9 @@ down(cell(X, Y, _), NextCell, State, N):-
     member(cell(NewX, Y, NextColor), State),
     NextCell = cell(NewX, Y, NextColor).
 
+%---------------------------------
 
+% base case to terminate when the goal cell is found and print the path
 search(Open, Closed, Goal,Board,N):-
     getBestState(Open, [CurrentState,Parent,G,H,F], _), % Step 1
     CurrentState = Goal, % Step 2
@@ -43,10 +52,12 @@ search(Open, Closed, Goal,Board,N):-
     findSolution(NewClosed, Goal, Acc, Solution), 
     printSolution(Solution), !.
     
+% base case to terminate if there is no cycle found
 search([], _, _, _,_) :-
     write("Search is complete!"), nl,
     write("No Path is found!"), !.
 
+% recursive case 
 search(Open, Closed, Goal,Board,N):-
     getBestState(Open, CurrentNode, TmpOpen),
     getAllValidChildren(CurrentNode,TmpOpen,Closed,Goal,Children,Board,N), % Step 3
@@ -67,7 +78,7 @@ getBestState(Open, BestChild, Rest):-
     findMin(Open, BestChild),
     delete(Open, BestChild, Rest).
 
-
+% find the minimum heuristic value from the open list
 findMin([X], X):- !.
 findMin([Head|T], Min):-
     findMin(T, TmpMin),
@@ -89,12 +100,13 @@ memberButBetter(Next, List, NewF):-
     min_list(Numbers, MinOldF),
     MinOldF > NewF.
 
-
+% get the path after the goal is found
 findSolution(_, null, Solution, Solution).
 findSolution(List, Cell, Accum, Solution):-
     member([Cell, Parent, _, _, _], List),
     findSolution(List, Parent, [Cell|Accum], Solution).
 
+% print the path
 printSolution([Cell]):- write(Cell), !.
 printSolution([Cell|Rest]) :-
     write(Cell), write(' -> '),
